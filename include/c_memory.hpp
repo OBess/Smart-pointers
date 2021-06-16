8f60bc2dcfbdc06f921344b2e90b26a7dc168e66
OBess <dima.suschenko03@gmail.com>
2021-06-16 11:20:52 +0300

    Init MyUnique_ptr and MyShared_ptr

---
+++ include/c_memory.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef C_MEMORY_HPP
+#define C_MEMORY_HPP
+
+// Custom
+#include "c_memory.hpp"
+
+class MyUnuque_ptr
+{
+public:
+
+private:
+
+};
+
+class MyShared_ptr
+{
+public:
+
+private:
+
+};
+
+#endif //C_MEMORY_HPP
