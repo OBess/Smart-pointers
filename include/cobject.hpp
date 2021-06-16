a9f269d247648dcddd4fecbf38ca20d604b4868c
OBess <dima.suschenko03@gmail.com>
2021-06-16 11:45:22 +0300

    Added I/O operators for basic_stream

--- include/cobject.hpp
+++ include/cobject.hpp
@@ -2,11 +2,37 @@
 #ifndef COBJECT_HPP
 #define COBJECT_HPP
 
+// C++
+#include <iostream>
+
 class CObject
 {
 public:
-   CObject() = default;
-   virtual ~CObject() = default;
+   CObject() noexcept = default;
+   virtual ~CObject() noexcept = default;
+
+   friend std::basic_ostream<char> &operator<<(std::basic_ostream<char> &os, const CObject &object)
+   {
+      os << object.output();
+      return os;
+   }
+
+   friend std::basic_istream<char> &operator>>(std::basic_istream<char> &in, CObject &object)
+   {
+      object.input(in);
+      return in;
+   }
+
+   virtual std::string toString() const noexcept = 0;
+
+protected:
+   virtual void input(std::basic_istream<char> &in) = 0;
+
+private:
+   std::string output() const noexcept
+   {
+      return this->toString();
+   }
 };
 
 #endif //COBJECT_HPP
